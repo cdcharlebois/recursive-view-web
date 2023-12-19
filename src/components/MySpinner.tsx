cfa0bbc14261b3a18d2011663c9daa92d77adaa1
Conner Charlebois <cdcharlebois@gmail.com>
2023-12-19 08:35:39 -0500

    sorta loading content

---
+++ src/components/MySpinner.tsx
@@ -0,0 +1,23 @@
+import { createElement } from "react";
+import "../ui/MySpinner.css";
+
+const MySpinner = () => {
+    return (
+        <div className="lds-spinner">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    );
+};
+
+export default MySpinner;
